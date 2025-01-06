e0f59a0285b8ea4d13cb506368e31b2eaf033f94
Mohammed Ismail <mahammedismail160@gmail.com>
2025-01-06 16:30:39 +0300

    Add initial implementation of JSDOM project with HTML, CSS, and JavaScript files

---
+++ Day3/JSDOM.js
@@ -0,0 +1,25 @@
+const TheH1 = document.getElementById("theH1");
+const Thediv = document.getElementById("theDiv");
+const Thep = document.getElementById("theP");
+const Thediv2 = document.getElementById("theDiv2");
+const Thediv3 = document.getElementById("theDiv3");
+const Thediv4 = document.getElementById("theDiv4");
+const Thebutton = document.getElementById("theButton");
+const Thebutton2 = document.getElementById("theButton2");
+const Thebutton3 = document.getElementById("theButton3");
+
+
+
+TheH1.onclick = function (){
+    TheH1.innerHTML = "Best Bootcamp";
+} ;
+
+Thebutton.onclick = function (){
+    Thep.innerHTML = "Mohammed Ismail";
+} ;
+Thebutton2.onclick = function (){
+    Thep.innerHTML = "13";
+} ;
+Thebutton3.onclick = function (){
+    Thep.innerHTML = "ASTU";
+} ;
